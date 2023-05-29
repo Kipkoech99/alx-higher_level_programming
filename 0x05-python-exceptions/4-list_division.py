77d0b40638bd3eea4262a72bb7ac92191af7e373
Kipkoech99 <kipkoechevans99@gmail.com>
2023-05-29 11:16:28 -0700

    Fixed wc

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
@@ -17,5 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         finally:
             new_list.append(div)
-    return (new_list)
-
+            return (new_list)
