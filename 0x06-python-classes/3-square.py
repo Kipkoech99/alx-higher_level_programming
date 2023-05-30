85e2172c20eb32af07379f733e98c690bd7c774e
Kipkoech99 <kipkoechevans99@gmail.com>
2023-05-30 08:42:46 -0700

    End of mandatory tasks

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """Define a square."""
+
+    def __init__(self, size=0):
+        """Square.
+
+        Args:
+            size (int): Square atrribute.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return current square area"""
+        return (self.__size * self.__size)
