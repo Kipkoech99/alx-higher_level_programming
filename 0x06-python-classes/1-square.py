a720f2b1edc5e83249cc0dcc656fb83e5320180c
Kipkoech99 <kipkoechevans99@gmail.com>
2023-05-30 08:12:42 -0700

    Square class and initialization

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class square"""
+
+class Square:
+    """Defines a square based on 0-square.py"""
+    def __init__(self, size):
+        """Square initialization
+
+        Args:
+            int(size): attribute.
+        """
+        self.__size = size
