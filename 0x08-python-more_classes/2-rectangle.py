66c113a846fbfa5ab91a67795fa1c4f5ef9e0ee5
Kipkoech99 <kipkoechevans99@gmail.com>
2023-06-05 06:46:53 -0700

    More task commits

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,3 +47,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Returns the area of rectangle"""
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,3 +47,29 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Returns the area of rectangle"""
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self) -> str:
+        """print the rectangle with the character #"""
+
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
+
