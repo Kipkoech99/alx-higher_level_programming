4ea0700af42da25e38b07e0560cc26a8455f007d
Kipkoech99 <kipkoechevans99@gmail.com>
2023-06-12 11:24:05 -0700

    First two submits

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+    A module that returns a list of available attributes
+    and methods of an object
+"""
+def lookup(obj):
+    """A function that returns all attributes and methods"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+
+"""
+    A module that inherits from a list class
+"""
+
+class MyList(list):
+    """A class that is a child to list"""
+    def print_sorted(self):
+        """Method to sort list"""
+        sorted_order = sorted.list
+        print(sorted_order)
